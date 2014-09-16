e844cf58f040f2987933983cae156d0915115e48
jkramer26 <jkramer26@L11716042779.wctc.msft>
2014-09-16 16:02:03 -0500

    First Save

---
+++ src/dip/lab2/TypeSafe.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dip.lab2;
+
+/**
+ *
+ * @author jkramer26
+ */
+public class TypeSafe {
+    
+    
+    //this is a constant
+//    private final int MAX = 5;
+//    
+//    //no guranttees that red or blue will be entered or correctly entered. 
+//    //this is unsafe. If you use a string parameter you are using unsafe code. 
+//    
+//    //you enum short for enumerated constant. Says that wel'll define some values and you have to pick one
+//    //you don't have to spell it you just pick one
+//    
+//    //you can create an enum class. Go to new class enum other. 
+//    
+//    
+////    public void chooseTeam(String color) {
+////        if (color.equals("Red")) {
+////            System.out.println("You are on the Patriots team");
+////        } else if (color.equals("Blue")) {
+////            System.out.println("You are on the smurf team");
+////        }
+////    }
+//    
+//    public void chooseTeam(NewEnum color) {
+//        if (color.equals("Red")) {
+//            System.out.println("You are on the Patriots team");
+//        } else if (color.equals("Blue")) {
+//            System.out.println("You are on the smurf team");
+//        }
+//    }
+//    
+//    public static void main(String[] args) {
+//        chooseTeam(NewEnum.RED);
+//    }
+}
